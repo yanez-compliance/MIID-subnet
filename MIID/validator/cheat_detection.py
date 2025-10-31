212bb9792ace352b711b50a997ac5aa311d48e49
Omar-A-Abaza <omar.aabaza.1@gmail.com>
2025-10-31 19:08:23 -0400

    both cheat detections

--- MIID/validator/cheat_detection.py
+++ MIID/validator/cheat_detection.py
@@ -372,6 +372,7 @@ def detect_cheating_patterns(
                     unique_words = set(parts)
                     dedup_text = " ".join(unique_words)
                     letters = re.findall(r'[^\W\d]', dedup_text, flags=re.UNICODE)
+                    letters = [c for c in letters if c not in ['\u02BB', '\u02BC']]
                     normalized = ''.join(sorted(letters)).lower()
                     normalized_addresses.append(normalized)
                 miner_address_sets[name] = set(normalized_addresses)
